86a8183ca6d23997c0331d36ffdcd7d0d4d54940
gioruggieri <21227260+gioruggieri@users.noreply.github.com>
2025-12-24 13:33:09 +0100

    ..

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/pdf": ["./node_modules/@sparticuz/chromium/**/*"],
+    "/api/pdf/route": ["./node_modules/@sparticuz/chromium/**/*"],
   },
 };
 
