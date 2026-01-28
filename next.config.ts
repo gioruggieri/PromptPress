b287133ea6b3d5422ea1023c08f7ef2f10208571
gioruggieri <21227260+gioruggieri@users.noreply.github.com>
2026-01-28 11:18:36 +0100

    next.config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/pdf": ["./node_modules/@sparticuz/chromium/**/*"],
     "/api/pdf/route": ["./node_modules/@sparticuz/chromium/**/*"],
+    "/api/docx-pandoc": [
+      "./node_modules/pandoc-bin/**/*",
+      "./node_modules/bin-wrapper/**/*",
+      "./node_modules/os-filter-obj/**/*",
+    ],
+    "/api/docx-pandoc/route": [
+      "./node_modules/pandoc-bin/**/*",
+      "./node_modules/bin-wrapper/**/*",
+      "./node_modules/os-filter-obj/**/*",
+    ],
   },
 };
 
